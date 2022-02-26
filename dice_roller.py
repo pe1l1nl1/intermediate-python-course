70a9d8bebbfa81aa23321a799edbb4811943d554
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2022-02-26 19:19:32 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
 def main():
   print('You rolled a die')
+  
+
 
 if __name__== "__main__":
   main()
+  
+  roll = 5
+print(f'You rolled a {roll}')
+
+import random 
+
+roll = random.randint(1,6);
